204e8ea7a3066ed59a4f9d064b3e2f50d03c5af2
Tarik-Khalfi <khtarek07@gmail.com>
2021-10-01 12:01:26 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom'
+import { Provider } from 'react-redux';
+import './index.css';
+import App from './App';
+import store from './app/store'
+import 'antd/dist/antd.css'
+const root = document.getElementById('root')
+ReactDOM.render(
+    <Router>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </Router>,
+    root);
+
